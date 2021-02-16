3365dd5945e51b7c11292160468facee3c8ae65d
EricM95 <eric.man95@gmail.com>
2021-02-16 11:08:10 +0000

    Completed week 3 logbook

---
+++ Week 1/wk1ex7.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+<?php
+	$hourlyrate	= 5.75;
+	$hoursperweek = 40;
+	$taxrate22 = 0.78;
+	$gross = $hourlyrate * $hoursperweek;
+	$net = $gross * $taxrate22;
+	//echo $gross;
+	echo $net;
+?>
+</body>
+</html>
