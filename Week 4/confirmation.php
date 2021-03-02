05c95dfc190ba406eace5fa8321d6a87cf919e88
EricM95 <eric.man95@gmail.com>
2021-03-02 11:35:41 +0000

    Edited confirmation php

--- Week 4/confirmation.php
+++ Week 4/confirmation.php
@@ -5,10 +5,10 @@ $qty = $_SESSION['selqty'];
 $total = $qty * $price; 
 switch ($_GET['size'])
 {
-   
+   case "Small" : 
 }
    echo "<h2> Your order qty is " . $_SESSION['selqty']  . "</h2></br>";
    echo "<h2> and the selected colour is $_POST[selcolour].</h2></br>";
   // echo "<h2> Your order total is £" . $total . " </h2>";
- print_r($_SESSION);
+// print_r($_SESSION);
 ?>
