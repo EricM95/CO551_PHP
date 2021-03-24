1fc29987029e9e20ecac0de2ff56c7e4e8aaf499
EricM95 <eric.man95@gmail.com>
2021-03-24 05:27:21 +0000

    wk6ex3 begin

--- Week 6/wk6ex2show.php
+++ Week 6/wk6ex2show.php
@@ -2,7 +2,14 @@
     // Connect and select database
     $link = mysqli_connect("localhost", "root", "", "db1_21815369");
 
-    $sql = "UPDATE test (name, phone_number, email)
-    VALUES ('$_POST[txtname]', '$_POST[txttelno]', '$_POST[txtemail]')";
+    $sql = "UPDATE test 
+    SET ('$_POST[txtname]', '$_POST[txttelno]', '$_POST[txtemail]')
+    WHERE name, phone_number, email";
     $result = mysqli_query($link, $sql);
+
+    if (mysqli_query($link, $sql)) {
+        echo "Record updated successfully";
+      } else {
+        echo "Error updating record: " . mysqli_error($link);
+      }
 ?>
