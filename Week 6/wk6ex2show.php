952b4e8941dfc507a2238acd7a17f8bd62bff0fc
EricM95 <eric.man95@gmail.com>
2021-03-27 21:32:28 +0000

    Editing Week 6 exercise 3

--- Week 6/wk6ex2show.php
+++ Week 6/wk6ex2show.php
@@ -2,9 +2,18 @@
     // Connect and select database
     $link = mysqli_connect("localhost", "root", "", "db1_21815369");
 
+    $id = $_GET['id'];
+    if(isset($_GET['id'])){
+      $id = $_GET['id'];
+    }
+    
+    $name = $_POST["txtname"];
+    $phone_number = $_POST["txttelno"];
+    $email = $_POST["txtemail"];
+
     $sql = "UPDATE test 
-    SET ('$_POST[txtname]', '$_POST[txttelno]', '$_POST[txtemail]')
-    WHERE name, phone_number, email";
+    SET  phone_number='$phone_number', email='$email'
+    WHERE name='$id'";
     $result = mysqli_query($link, $sql);
 
     if (mysqli_query($link, $sql)) {
